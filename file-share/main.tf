d9e09a18723555ec1feba8836f5f31bdd12f0e28
Namesh Sanjitha <tns.xiii@gmail.com>
2020-09-05 12:02:20 +1000

    Added a file share module for deploying SMB shares in a given storage account

---
+++ file-share/main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_storage_share" "this" {
+  name                 = "${var.name}"
+  storage_account_name = var.storage_account_name
+  quota                = 50
+}
+
---
+++ file-share/variables.tf
@@ -0,0 +1,14 @@
+variable "name" {
+  description = "vNet name"
+  type        = string
+}
+
+variable "resource_group" {
+  description = "Resource group name"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Storage account name"
+  type        = string
+}
